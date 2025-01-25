ac7b79b037d0d99d19de6a1440a2e1019a83b5cc
her24770 <her24770@uvg.edu.gt>
2025-01-25 13:48:17 -0600

    Cracion metodos y atributos de clase Node

--- Node.java
+++ Node.java
@@ -1,3 +1,36 @@
 public class Node {
-    private
+    private Node nextNode;
+    private Node previousNode;
+    private int value;
+
+    public Node(Node nextNode, Node previousNode, int value) {
+        this.nextNode = nextNode;
+        this.previousNode = previousNode;
+        this.value = value;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public Node getPreviousNode() {
+        return previousNode;
+    }
+
+    public void setPreviousNode(Node previousNode) {
+        this.previousNode = previousNode;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
 }

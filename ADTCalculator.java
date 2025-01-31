0339dad5fc4e0b5da025c5f54717ffda192b241c
her24770 <her24770@uvg.edu.gt>
2025-01-30 18:45:08 -0600

    Se creo clase calculator

---
+++ ADTCalculator.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ADTCalculator {
+    public void calculate(String file) {
+        Stack<Integer> stack = new Stack<>();
+        
+        ArrayList<ArrayList<String>> documentLines = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("datos.txt"))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                ArrayList<String> prefixOperation = new ArrayList<>();
+                for (char str : line.toCharArray()) {
+                    if (str != ' ') {
+                        prefixOperation.add(String.valueOf(str));
+                    }
+                }
+                documentLines.add(prefixOperation);
+            }
+        } catch (IOException e) {
+            System.out.println("Error en lectura del documento: " + e.getMessage());
+        }
+
+        for (ArrayList<String> line : documentLines) {
+
+            for (String str : line) {
+                if (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/") || str.equals("%")) {
+                    int value2 = stack.pop();
+                    int value1 = stack.pop();
+                    stack.push(stack.operation(str.charAt(0), value1, value2));
+                } else {
+                    stack.push(Integer.parseInt(str));
+                }
+            }
+
+            System.out.println("\nOperación:" + line);
+
+            System.out.println("Resultado: " + stack.pop());
+
+        }
+
+    }
+}
--- Main.java
+++ Main.java
@@ -1,58 +1,9 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
-        Stack<Integer> stack = new Stack<>();
+        ADTCalculator calculator = new ADTCalculator();
+        calculator.calculate("datos.txt");
         
-        ArrayList<ArrayList<String>> profixExpresions = leerTxt();
-        //prueba funcionamiento
-        for (ArrayList<String> line : profixExpresions) {
-
-            for (String str : line) {
-                if (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/") || str.equals("%")) {
-                    int value2 = stack.pop();
-                    int value1 = stack.pop();
-                    stack.push(stack.operation(str.charAt(0), value1, value2));
-                } else {
-                    stack.push(Integer.parseInt(str));
-                }
-            }
-
-            System.out.println("\nOperación:" + line);
-
-            System.out.println("Resultado: " + stack.pop());
-
-        }
-
-
-
-
-    }
-
-    public static ArrayList<ArrayList<String>> leerTxt() {
-        ArrayList<ArrayList<String>> documentLines = new ArrayList<>();
-
-        try (BufferedReader br = new BufferedReader(new FileReader("datos.txt"))) {
-            String line;
-
-            while ((line = br.readLine()) != null) {
-                ArrayList<String> prefixOperation = new ArrayList<>();
-                for (char str : line.toCharArray()) {
-                    if (str != ' ') {
-                        prefixOperation.add(String.valueOf(str));
-                    }
-                }
-                documentLines.add(prefixOperation);
-            }
-        } catch (IOException e) {
-            System.out.println("Error en lectura del documento: " + e.getMessage());
-        }
-
-        return documentLines;
     }
     
 }
